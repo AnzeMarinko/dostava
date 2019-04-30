834edffeb0becb82282fe3b1a15210802e8d8c2c
AnzeMarinko <anze.marinko96@gmail.com>
2019-04-30 21:16:17 +0200

    uvoz stanja in testna stanja za uvoz

--- podatkovne_strukture.py
+++ podatkovne_strukture.py
@@ -1,96 +1,154 @@
 # definicije objektov polje (tipa garaza, pot, skladisce, trg, ovira), robot in stanje
 # razred Polje ima tip in atribute, ki povedo, nekaj lastnosti o polju odvisno od tipa
 class Polje:
-    def __init__(self, tip, atributi):
-        self.tip = tip
-        self.atributi = atributi
+	def __init__(self, tip, atributi=None):
+		self.tip = tip
+		self.atributi = atributi
 		# polja tipa garaza in pot imajo le to lastnost, da lahko vsebujejo nekega robota ali pa ne,
 		# garaza je posebna le v tem, da morajo biti na koncu vsi robotki v garazi,
 		# polja tipa trg vsebujejo slovar s povprasevanjem (blago in kolicina),
 		# polja tipa skladisce vsebujejo slovar s ponudbo (blago in kolicina),
 		# polja tipa ovira ne dovoljujejo premika robota na to polje
-        if (tip in "garaza" or tip in 'pot') and type(atributi) is not Robot:
-            raise Exception("{} ne vsebuje atributa tipa robot.".format(tip))
-        elif (tip = "trg" or tip in 'skladisce') and len(atributi) != 2 and (type(atributi) is not dict):
-            raise Exception("{} ne vsebuje atributa tipa dict.".format(tip))
+		if (tip in "garaza" or tip in 'pot') and atributi and type(atributi) is not Robot:
+			raise Exception("{} ne vsebuje atributa tipa robot.".format(tip))
+		elif (tip in "trg" or tip in 'skladisce') and (type(atributi) is not dict):
+			raise Exception("{} ne vsebuje atributa tipa dict.".format(tip))
 		elif tip not in ['garaza', 'pot', 'skladisce', 'trg', 'ovira']:
 			raise Exception("Polja tipa {} ne obstajajo.".format(tip))
-    '''
+
+	def __repr__(self):
+		if self.tip in "garaza":
+			return "Garaza ({})".format(self.atributi)
+		elif self.tip in "pot":
+			return "Pot ({})".format(self.atributi)
+		elif self.tip in "trg":
+			return "Trg {}".format(self.atributi)
+		elif self.tip in "skladisce":
+			return "Skladisce {}".format(self.atributi)
+		elif self.tip in "ovira":
+			return "Ovira"
+	'''
 	mozna definicija funkcije dostavi, ce dovolimo, da robotu ostane nekaj blaga:
-    def dostavi(self, blago_1):
-        vrni = self.atributi[blago_1[0]] - blago_1[1]
-        self.atributi[blago_1[0]] = max([vrni, 0])
-        return ('' if max(-vrni, 0) == 0 else blago_1[0], max(-vrni, 0)) '''
+	def dostavi(self, blago_1):
+		vrni = self.atributi[blago_1[0]] - blago_1[1]
+		self.atributi[blago_1[0]] = max([vrni, 0])
+		return ('' if max(-vrni, 0) == 0 else blago_1[0], max(-vrni, 0)) '''
 	
 	# dostavi blago na trg, kolikor ima robot blaga prevec, zavrzi
-    def dostavi(self, blago_1):
-        if tip != "trg":
-            raise Exception("Blaga ni mogoce dostaviti na polje, ki ni trg.")
-        vrni = self.atributi[blago_1[0]] - blago_1[1]
-        self.atributi[blago_1[0]] = max([vrni, 0])
-        return ('', 0)
-    
+	def dostavi(self, blago_1):
+		if self.tip != "trg" or len(blago_1[0]) == 0:
+			raise Exception("Blaga ni mogoce dostaviti (polje ni trg ali robot nima blaga).")
+		vrni = self.atributi[blago_1[0]] - blago_1[1]
+		self.atributi[blago_1[0]] = max([vrni, 0])
+		return ('', 0)
+
 	# prevzemi neko kolicino blaga iz skladisca
-    def prevzemi(self, blago, kolicina):
-        if tip != "skladisce":
-            raise Exception("Prevzeti zelis s polja, ki ni skladisce.")
-        kolicina = min([self.atributi[blago], kolicina])
-        self.atributi[blago] -= kolicina
-        return (blago, kolicina)
+	def prevzemi(self, blago, kolicina):
+		if self.tip != "skladisce":
+			raise Exception("Prevzeti zelis s polja, ki ni skladisce.")
+		kolicina = min([self.atributi[blago], kolicina])
+		self.atributi[blago] -= kolicina
+		return (blago, kolicina)
 
 # razred robot vsebuje lastnosti o najvecji kolicini blaga (prostor),
 # vsebovanem blagu (tip blaga, kolicina) in polozaju na plosci (i, j)
 class Robot:
-    def __init__(self, prostor, blago, polozaj):
-        self.prostor = prostor
-        self.blago = blago
-        self.polozaj = polozaj
-        if  (type(prostor) is not int) or (type(blago[0]) is not str or type(blago[1]) is not int) 
-        or (type(polozaj[0]) is not int or type(polozaj[1]) is not int):
-            raise Exception("Blago ne vsebuje atributa tipa (str, int).")			
-	
+	def __init__(self, prostor, polozaj, blago):
+		self.prostor = prostor
+		self.polozaj = polozaj
+		self.blago = blago
+		if  (type(prostor) is not int) or (type(blago[0]) is not str or type(blago[1]) is not int) or (type(polozaj[0]) is not int or type(polozaj[1]) is not int):
+			raise Exception("Blago ne vsebuje atributa tipa (str, int).")			
+
+	def __repr__(self):
+		return "Robot ({} <= {}, {})".format(self.blago, self.prostor, self.polozaj)
+
 	# odlozi vsebovano blago na trgu
-    def odlozi(self, trg_1):
-        self.blago = trg_1.dostavi(self.blago)
-	
+	def odlozi(self, trg_1):
+		self.blago = trg_1.dostavi(self.blago)
+
 	# nalozi neko kolicino blaga iz skladisca
-    def nalozi(self, skladisce_1, blago, kolicina):
-        self.blago = skladisce_1.prevzemi(blago, min([kolicina, self.prostor]))
+	def nalozi(self, skladisce_1, blago, kolicina):
+		self.blago = skladisce_1.prevzemi(blago, min([kolicina, self.prostor]))
 
 # stanje vsebuje tabelo polj in seznam robotov
 class Stanje:
 	# definicija zacetnega stanja
 	# na poljih se ne rabimo podatka o zasedenosti z roboti
 	def __init__(self, polja, roboti):
-        self.polja = polja
+		self.polja = polja
 		self.roboti = roboti
 		for robot in roboti:
-			if self.polja[robot.polozaj[0],robot.polozaj[1]].tip is in ['garaza', 'pot']:
-				self.polja[robot.polozaj[0],robot.polozaj[1]].atributi = robot
+			if self.polja[robot.polozaj[0]][robot.polozaj[1]].tip in ['garaza', 'pot']:
+				self.polja[robot.polozaj[0]][robot.polozaj[1]].atributi = robot
 			else:
 				print('Robot {} ne more biti postavljen na izbrano polje'.format(robot))
-	
+
+	def __repr__(self):
+		opis = "Stanje:    " + str(self.roboti) + "\n"
+		for vrstica in self.polja:
+			opis += str(vrstica) + "\n"
+		return opis
+
 	# TODO:
 	# preveri, ce je konec (vsi trgi brez povprasevanj in roboti v garazah)
 	# dovoljene poteze (vsi mozni premiki robotov z upostevanjem razlicnih moznih nalaganj)
+	# ce gre robot na polje tipa skladisce ali trg le opravi nalaganje ali odlaganje in ostane na istem mestu
 
-# TODO:
-# uvoz stanja iz tekstovne datoteke oblike:
-# 
-# tip atributi,tip atributi,tip atributi, ... ,tip atributi
-# tip atributi,tip atributi,tip atributi, ... ,tip atributi
-# tip atributi,tip atributi,tip atributi, ... ,tip atributi
-#
-# prostor, blago, kolicina, i, j
-# prostor, blago, kolicina, i, j
-# prostor, blago, kolicina, i, j
-#
+stanje = Stanje([[Polje("garaza",None),Polje("pot",None),Polje("pot",None)],[Polje("pot",None),Polje("ovira"),Polje("pot",None)],[Polje("trg",{"moka":3,"voda":2,"jajca":4}),Polje("ovira"),Polje("skladisce",{"moka":1,"voda":1})]],[Robot(2,(0,0),("",0))])
+print(stanje)
+
+def uvozi_stanje(filename):
+	plosca = []
+	roboti = []
+	with open(filename,"r") as f:
+		print(f.readline())
+		f.readline()
+		while True:
+			polja = [polje.split(" ") for polje in f.readline()[:-1].split(",")]
+			if len(polja[0][0]) == 0:
+				break
+			vrstica = []
+			for polje in polja:
+				if len(polje) == 1:
+					vrstica.append(Polje(polje[0]))
+				else:
+					vrstica.append(Polje(polje[0],{blago:int(kolicina) for blago,kolicina in [x.split(".") for x in polje[1:]]}))
+			plosca.append(vrstica)
+		while True:
+			robot = f.readline()[:-1].split(",")
+			if len(robot[0]) == 0:
+				break
+			roboti.append(Robot(int(robot[0]),(int(robot[1]),int(robot[2])),(robot[3],int(robot[4]))))
+	return Stanje(plosca,roboti)
+			
+
+stanje = uvozi_stanje("testni_primeri/test1.aa")
+print(stanje)
+
+# uvoz iz tekstovne datoteke oblike:
+'''
+pomenljiv naslov stanja
+
+tip atributi,tip atributi,tip atributi, ... ,tip atributi
+tip atributi,tip atributi,tip atributi, ... ,tip atributi
+tip atributi,tip atributi,tip atributi, ... ,tip atributi
+
+prostor, i, j, blago, kolicina
+prostor, i, j, blago, kolicina
+prostor, i, j, blago, kolicina
+
+'''
 # kjer je tip dejanski tip, atributi pa so s presledki locene vrednosti blago.kolicina, kjer je to potrebno
-
 # npr.:
-# garaza, pot, skladisce moka.3 jajca.5
-# garaza, pot, pot
-# trg moka.2 jajca.3 mleko.4, pot, skladisce mleko.6
-#
-# 3, , , 0, 0
-# 3, , , 1, 0
+'''
+majhen test
+
+garaza, pot, skladisce moka.3 jajca.5
+garaza, pot, pot
+trg moka.2 jajca.3 mleko.4, pot, skladisce mleko.6
+
+3,0,0,,0
+3,1,0,,0
+
+'''
